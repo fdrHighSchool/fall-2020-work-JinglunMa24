b0da525482bb8d36d5cf0df6d13e54549f3cce59
Jinglun Ma <2461447099ma@gmail.com>
2020-10-19 18:45:04 -0400

    added assignments

---
+++ 03-bill_splitter/BillSpliter.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class BillSpliter{
+	public static void main(String[] args){
+
+		Scanner input = new Scanner(System.in);
+		System.out.println("Welcome to the Bill-Splitter");
+		System.out.print("Enter the total, pre-tax: ");
+		double total = input.nextInt();
+
+		System.out.print("How many people? ");
+		double numPeople = input.nextInt();
+
+		double perPerson = total / numPeople;
+
+		System.out.print("What is the percent of tax you are paying?");
+		double percentTax = input.nextInt();
+		double tax = (total * ((double)percentTax / 100));
+
+		System.out.print("How much is the tip are you paying?");
+		double tip = input.nextInt();
+
+		System.out.println("Each person must pay $" + perPerson + ", tax $" + tax + ", and tip $" + tip);
+
+	}//end main method
+}//end class

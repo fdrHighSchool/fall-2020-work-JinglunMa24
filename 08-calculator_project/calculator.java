bcfe6450bd32854e6552bdf2e258bd8dfa52a7fe
Jinglun Ma <2461447099ma@gmail.com>
2020-11-05 18:35:50 -0500

    Done with ALL

--- 08-calculator_project/calculator.java
+++ 08-calculator_project/calculator.java
@@ -22,23 +22,98 @@ public class calculator {
     String operator = exp.substring(firstS + 1, secondS);
     String operand1 = exp.substring(0, firstS);
       if (operand1.contains("/") && !operand1.contains("_")) {
-        operand1 = convertFraction(operand1);
+        operand1 = convertMix(operand1);
       } //end if operand1 has / and no _
       else if (!operand1.contains("_")){
         operand1 += "_0/1";
       } //end if only integer
     String operand2 = exp.substring(secondS + 1, exp.length());
       if (operand2.contains("/") && !operand2.contains("_")) {
-        operand2 = convertFraction(operand2);
+        operand2 = convertMix(operand2);
       } //end if operand2 has / and no _
       else if (!operand2.contains("_")){
         operand2 += "_0/1";
       } //end if only integer
-    return operand2;
+    if (operator.equals("+")) {
+      operand1 = convertFraction(operand1);
+      operand2 = convertFraction(operand2);
+      String result = calculationAdd(operand1, operand2);
+      return result;
+    } //end addition
+
+    else if (operator.equals("-")) {
+      operand1 = convertFraction(operand1);
+      operand2 = convertFraction(operand2);
+      String result = calculationMinus(operand1, operand2);
+      return result;
+    } //end subtraction
+
+    else if (operator.equals("*")) {
+      operand1 = convertFraction(operand1);
+      operand2 = convertFraction(operand2);
+      String result = calculationDivide(operand1, operand2);
+      return result;
+    } //end multiplication
+
+    else if (operator.equals("/")) {
+      operand1 = convertFraction(operand1);
+      operand2 = convertFraction(operand2);
+      String result = calculationMultiply(operand1, operand2);
+    } //end division
+
+    return "Error";
 
   } //end produceAnswer()
 
-  public static String convertFraction(String fraction) {
+  public static String calculationAdd(String firstF, String secondF) {
+    String numer1 = getNumer1(firstF);
+    String denom1 = getDenom1(firstF);
+    String numer2 = getNumer2(secondF);
+    String denom2 = getDenom2(secondF);
+    int Fnumer = Integer.parseInt(numer1) * Integer.parseInt(denom2) + Integer.parseInt(numer2) * Integer.parseInt(denom1);
+    int Fdenom = Integer.parseInt(denom1) * Integer.parseInt(denom2);
+    String result = reduceFraction(Fnumer, Fdenom);
+    String Fresult = convertMix(result);
+    return Fresult;
+  } //end addition()
+
+  public static String calculationMinus(String firstF, String secondF) {
+    String numer1 = getNumer1(firstF);
+    String denom1 = getDenom1(firstF);
+    String numer2 = getNumer2(secondF);
+    String denom2 = getDenom2(secondF);
+    int Fnumer = Integer.parseInt(numer1) * Integer.parseInt(denom2) - Integer.parseInt(numer2) * Integer.parseInt(denom1);
+    int Fdenom = Integer.parseInt(denom1) * Integer.parseInt(denom2);
+    String result = reduceFraction(Fnumer, Fdenom);
+    String Fresult = convertMix(result);
+    return Fresult;
+  } //end subtraction()
+
+  public static String calculationMultiply(String firstF, String secondF) {
+    String numer1 = getNumer1(firstF);
+    String denom1 = getDenom1(firstF);
+    String numer2 = getNumer2(secondF);
+    String denom2 = getDenom2(secondF);
+    int Fnumer = Integer.parseInt(numer1) * Integer.parseInt(numer2);
+    int Fdenom = Integer.parseInt(denom1) * Integer.parseInt(denom2);
+    String result = reduceFraction(Fnumer, Fdenom);
+    String Fresult = convertMix(result);
+    return Fresult;
+  } //end multiplication()
+
+  public static String calculationDivide(String firstF, String secondF) {
+    String numer1 = getNumer1(firstF);
+    String denom1 = getDenom1(firstF);
+    String numer2 = getNumer2(secondF);
+    String denom2 = getDenom2(secondF);
+    int Fnumer = Integer.parseInt(numer1) * Integer.parseInt(denom2);
+    int Fdenom = Integer.parseInt(denom1) * Integer.parseInt(numer2);
+    String result = reduceFraction(Fnumer, Fdenom);
+    String Fresult = convertMix(result);
+    return Fresult;
+  } //end division
+
+  public static String convertMix(String fraction) {
     int divide = fraction.indexOf("/");
     String numer = fraction.substring(0, divide);
     String denom = fraction.substring(divide + 1, fraction.length());
@@ -52,9 +127,37 @@ public class calculator {
     return result;
   } //end determineFraction()
 
-  public static void calculationAdd(String firstF, String secondF) {
+  public static String convertFraction(String fraction) {
+    int lowDash = fraction.indexOf("_");
+    int divide = fraction.indexOf("/");
+    String whole = fraction.substring(0, lowDash);
+    String numer = fraction.substring(lowDash + 1, divide);
+    String denom = fraction.substring(divide + 1, fraction.length());
+    int Fnumer = Integer.parseInt(whole) * Integer.parseInt(denom) + Integer.parseInt(numer);
+    String result = String.valueOf(Fnumer) + "/" + denom;
+    return result;
+  } //end convertFraction();
 
-  } //end calculation()
+  public static String getNumer1(String fraction1) {
+    int divide = fraction1.indexOf("/");
+    String numer1 = fraction1.substring(0, divide);
+    return numer1;
+  } //end get numer1
+  public static String getDenom1(String fraction1) {
+    int divide = fraction1.indexOf("/");
+    String denom1 = fraction1.substring(divide + 1, fraction1.length());
+    return denom1;
+  } //end get denom1
+  public static String getNumer2(String fraction2) {
+    int divide = fraction2.indexOf("/");
+    String numer2 = fraction2.substring(0, divide);
+    return numer2;
+  } //end get numer2
+  public static String getDenom2(String fraction2) {
+    int divide = fraction2.indexOf("/");
+    String denom2 = fraction2.substring(divide + 1, fraction2.length());
+    return denom2;
+  } //end get denom2
 
   public static String reduceFraction(int x, int y) {
     int d;

102a71c66c618d658007abc3daa6a4f789d1c716
Jinglun Ma <2461447099ma@gmail.com>
2020-12-06 16:48:29 -0500

    mean and median done, mode is bugged

--- Grades.java
+++ Grades.java
@@ -14,15 +14,53 @@ public class Grades {
     } //end loop
 
     System.out.println(Arrays.toString(grades));
-    System.out.println(getAverage(grades));
+    System.out.println("The average of the grades is: " + getAverage(grades));
+    System.out.println("The median of the grades is: " + getMedian(grades));
+    System.out.println("The mode of the grades is: " + getMode(grades));
   } //end main
 
   public static double getAverage(int[] grades) {
-    int sum = 0;
+    double sum = 0;
+
     for (int i = 0; i < grades.length; i++) {
       sum += grades[i];
     } //end loop
+
     return sum / grades.length;
-  } //end getAverage
+  } //end getAverage()
+
+  public static double getMedian(int[] grades) {
+    Arrays.sort(grades);
+
+    if (grades.length % 2 == 0) {
+      double num1 = grades[grades.length / 2 - 1];
+      double num2 = grades[grades.length / 2];
+      return (num1 + num2) / 2;
+    } //end if grades are even
+
+    else {
+      return grades[grades.length / 2];
+    } //end else
+  } //end getMedian()
+
+  public static int getMode(int[] grades) {
+    int num = 0;
+    int count = 0;
+
+    for (int i = 0; i < grades.length; i++) {
+      int tempNum = grades[i];
+      int tempCount = 0;
+      for (int j = 0; i < grades.length; i++) {
+        if (grades[i] == grades[j]) {
+          tempCount++;
+        } //end if grades index of "i" is the same as grades index of "j"
+      } //end for loop for j
+      if (tempCount > count) {
+        num = tempNum;
+        count = tempCount;
+      } //end if count is > repeats
+    } //end for loop for i
+    return num;
+  } //end getMode()
 
 } //end class

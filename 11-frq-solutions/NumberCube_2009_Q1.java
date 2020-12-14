532026388b30ac025284c062a7f930360cb59fbf
Jinglun Ma <2461447099ma@gmail.com>
2020-12-13 21:01:34 -0500

    Cannot understand directions, wait for going over

---
+++ 11-frq-solutions/NumberCube_2009_Q1.java
@@ -0,0 +1,9 @@
+public class NumberCube_2009_Q1 {
+  public static void main(String[] args) {
+
+  } //end main
+
+  public static int[] getCubeTosses(NumberCube cube, int numTosses) {
+    return getCubeTosses;
+  } //end getCubeTosses()
+} //end class
---
+++ 11-frq-solutions/SelfDivisor_2007_Q1.java
@@ -0,0 +1,13 @@
+public class SelfDivisor_2007_Q1 {
+  public static void main(String[] args) {
+    System.out.print(isSelfDivisor(-1));
+  } //end main
+
+  public static boolean isSelfDivisor(int number) {
+    return (number > 0);
+  } //end isSelfDivisor()
+
+  public static int[] firstNumSelfDivisors(int start, int num) {
+    start > 0;
+  } //end firstNumSelfDivisors()
+} //end class

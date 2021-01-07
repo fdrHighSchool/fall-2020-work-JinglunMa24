f6b149102142b2b2f1f9b20cdf0a03985d8e1231
Jinglun Ma <2461447099ma@gmail.com>
2021-01-07 09:18:00 -0500

    added assignment

---
+++ 13-student/Student.java
@@ -0,0 +1,37 @@
+public class Student() {
+  // instance variables
+  private String name;
+  private String osis;
+  private int[] grades;
+
+  // constructor
+  
+
+  // other methods
+
+  public String getName() {
+
+
+  } //end getName()
+
+  public String getOSIS() {
+
+
+  } //end getOSIS()
+
+  public void setName() {
+
+
+  } //end setName()
+
+  public void addGrades(int arr) {
+
+
+  } //end addGrades()
+
+  public double calculateAverage() {
+
+
+  } //end calculateAverage()
+
+} //end class

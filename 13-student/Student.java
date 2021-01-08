3ac33ff724f9835513f61ec1a6fa1980f9f5eaaf
Jinglun Ma <2461447099ma@gmail.com>
2021-01-08 09:21:32 -0500

    added assignment

--- 13-student/Student.java
+++ 13-student/Student.java
@@ -1,26 +1,30 @@
-public class Student() {
+public class Student {
   // instance variables
   private String name;
   private String osis;
   private int[] grades;
 
   // constructor
-  
+  public Student(String n, String o) {
+    this.name = n;
+    this.osis = o;
+    grades = new int[10];
+  } //end Student()
 
   // other methods
 
   public String getName() {
 
-
+    return "";
   } //end getName()
 
   public String getOSIS() {
 
-
+    return "";
   } //end getOSIS()
 
-  public void setName() {
-
+  public void setName(String name) {
+    this.name = name;
 
   } //end setName()
 
@@ -31,7 +35,11 @@ public class Student() {
 
   public double calculateAverage() {
 
-
+    return 0.0;
   } //end calculateAverage()
+
+  public String toString() {
+    return "Student:" + this.name + " OSIS:" + this.osis;
+  } //end toString()
 
 } //end class
---
+++ 13-student/StudentDriver.java
@@ -0,0 +1,11 @@
+public class StudentDriver {
+  public static void main(String[] args) {
+    Student student1 = new Student("Jinglun", "239970163");
+
+    System.out.println(student1);
+
+    student1.setName("Ma");
+    System.out.println(student1);
+  } //end main
+
+} //end class

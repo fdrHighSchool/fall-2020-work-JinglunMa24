60cc657549d4740bb5f49a86976a119cea1deef1
JinglunMa24 <72706770+JinglunMa24@users.noreply.github.com>
2021-01-12 08:50:24 -0500

    Add files via upload

---
+++ 13-student/StudentDriver.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class StudentDriver {
+  public static void main(String[] args) {
+    Student student1 = new Student("Ma", "239970163");
+    Student student2 = new Student("Hello", "1266874531");
+    System.out.println(student1);
+    System.out.println(student2);
+
+    student1.setName("Jinglun");
+    System.out.println(student1);
+
+    System.out.println(student1.getName());
+    System.out.println(student1.getOSIS());
+
+    student1.addGrades(100);
+    student1.displayGrades();
+    student1.addGrades(90);
+    student1.displayGrades();
+
+    student2.addGrades(95);
+    student2.displayGrades();
+
+    Student student3 = new Student("Siri", "987654321");
+    student3.fillArray();
+    student3.displayGrades();
+
+  } //end main
+
+} //end class
